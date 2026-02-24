b6b9ccd7883f74e4dfe01dd9ae662397847cc7ae
Rodrigo Silverio <rodrigosilverio@Rodrigos-MacBook-Air.local>
2026-02-24 20:36:28 -0300

    update

--- src/app/admin/content/about/page.tsx
+++ src/app/admin/content/about/page.tsx
@@ -1,7 +1,8 @@
-import { getSectionContent } from "@/lib/content";
 import { AboutEditor } from "@/components/admin/editors/AboutEditor";
+import { getSectionContent } from "@/lib/content";
 
 export default async function AboutContentPage() {
   const data = await getSectionContent("about");
   return <AboutEditor initialData={data} />;
 }
+// tste
